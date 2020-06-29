ee65364d6755fba08eeccbaa08e05f45fa8034c9
summer <739923259@qq.com>
2020-06-29 21:32:50 +0800

    1

--- app/src/main/java/com/project/myapplicationsms/network/ApiUrlManager.java
+++ app/src/main/java/com/project/myapplicationsms/network/ApiUrlManager.java
@@ -3,5 +3,8 @@ public class ApiUrlManager {
     public static String BaseUrl = "http://testcrmapi.dpzaixian.com";//测试地址
     public static String API_LOGIN_URL = BaseUrl + "/api/v5/customer/login";
     public static String API_CRM_QINIU_SETTING = BaseUrl + "/api/v1/system/qiniu";//获取七牛云配置
+    public static String API_INNER_SORSI = BaseUrl + "/inner/sorsi";//心跳接口
+
+
 
 }
